dc2519a6b9ea74670ec7d02fb2b5ececdca20593
NoNoDu88 <65761225+ZombarDu88@users.noreply.github.com>
2022-12-28 15:50:19 +0100

    Update L4D2_presence.py

--- source/Py_folder/L4D2_presence.py
+++ source/Py_folder/L4D2_presence.py
@@ -9,12 +9,11 @@ def Discord_Rpc():
 
 	if output_discord.find("Discord.exe") != -1:
 
-		client_id = "1056228703073476731"
+		client_id = "XD no"
 		Rpc = Presence(client_id)
 		Rpc.connect()
 
-		Rpc.update(details="ver 1.1.5", state="Credits: NoNoDu88, FoxTroT, Kef", large_image="l4d2_map_selector_icon", start=epoch_time,
-		buttons=[{"label": "The Program\U0001F40D (1.1.5)" , "url": "https://github.com/ZombarDu88/Left-4-dead-2-Map-Selector"}])
+		Rpc.update()
 
 	else:
 		print("\033[0;31mDiscord is not launched no rpc\033[0;37m")
